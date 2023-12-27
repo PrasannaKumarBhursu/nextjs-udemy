f40648f8a36692937fa74988abd12d5b0c1aa3ae
Prasanna kumar <bhursuprasannakumar@gmail.com>
2023-12-27 16:25:58 +0530

    added signin and singup pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,13 @@ import '@/styles/globals.css'
 import { RecoilRoot } from "recoil";
 import type { AppProps } from 'next/app'
 import AppBar from '../components/AppBar';
+import InitUser from '@/components/InitUser';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
       <AppBar />
+      <InitUser />
       <Component {...pageProps} />
     </RecoilRoot>
   )
