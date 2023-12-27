d43fe0f4dcd7959c94e8e66728298f22d38de0c3
Prasanna kumar <bhursuprasannakumar@gmail.com>
2023-12-27 10:28:14 +0530

    added appbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import '@/styles/globals.css'
 import { RecoilRoot } from "recoil";
 import type { AppProps } from 'next/app'
+import AppBar from '../components/AppBar';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
+      <AppBar />
       <Component {...pageProps} />
     </RecoilRoot>
   )
